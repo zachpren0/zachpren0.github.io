9bedbd479898dbb6cf6984840256081ea9f1ce9e
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2023-06-16 18:37:08 -0700

    firewood summary page created

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,22 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
-window.addEventListener('DOMContentLoaded', event => {
+// Get all the collapsible elements
+var collapsibleElements = document.querySelectorAll('.collapse');
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+// Add event listeners to the buttons
+var buttons = document.querySelectorAll('[data-toggle="collapse"]');
+buttons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    // Close all the collapsible elements except the current one
+    collapsibleElements.forEach(function (element) {
+      var currentButton = document.querySelector(button.getAttribute('href'));
+      if (element !== currentButton) {
+        $(element).collapse('hide');
+        var unselectedButton = document.querySelector('a[href="' + '#' + element.getAttribute('id') + '"]');
+        unselectedButton.classList.remove('active'); // Remove the 'active' class from unselected buttons
+      } else {
+        button.classList.toggle('active'); // Toggle the 'active' class on the selected button
+      }
     });
-
+  });
 });
+
